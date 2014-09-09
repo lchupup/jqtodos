791625b403eb06b42d478b957af210c8a827a78b
Troublemaker <156894161@qq.com>
2014-09-09 13:35:11 +0800

    better js

--- jqtodos.js
+++ jqtodos.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	var addtxt = $("#addtxt");
 	var addcontent = $(".addcontent");
 	var bottom = $("#bottom");
+	/*************************项目输入**************************************************************/
 	addtxt.keypress(function(e){
 		 if (e.keyCode == 13) {
 		 	if (this.value != "") {
@@ -12,15 +13,28 @@ $(document).ready(function(){
 		 	 	$(".item_edit").last().val(this.value);
 		 	 	$("#item_list li").last().css("border","none");
 		 	 	$("#item_list li").last().prevAll().css("border-bottom","1px solid #ccc");
+		 	 	$("#item_left").html("还有"+$("#item_list li").length+"个未完成项目");
 		 	 	this.value = "";
 		 	 };
 		};
 	});
+	/***************************项目修改和删除*************************************************/
 	$("#item_list").on("mouseover","li",function(){
 		 $(this).find(".del").css("display","block");
 	}).on("dblclick","li",function(){
 		$(this).find(".item_manage").css("display","none");
-		$(this).find(".item_edit").css("display","block").select();
+		$(this).find(".item_edit").css("display","block").select().on("blur",function(){
+			if ($(this).val() != ""){
+				$(this).css("display","none");
+				$(this).prev().css("display","block").find(".item_name").html($(this).val());	
+			}else{
+				$(this).parents("li").remove();
+				if ($("#item_list li").length == 0) {
+					addcontent.css("display","none");
+				 	bottom.css("display","none");
+				};
+			}
+		});
 	});
 	$("#item_list").on("mouseout","li",function() {
 		 $(this).find(".del").css("display","none");
@@ -36,5 +50,40 @@ $(document).ready(function(){
 	});
 	$("#item_list").on("mouseout",".del",function() {
 		$(this).css("background","url(./image/destroy.png)");
+	});
+	/*******************************项目完成情况记录*******************************************************/
+	$("#alldone").click(function(){
+		if ($(this).prop("checked") == true){
+			$(".itemdone").prop("checked",true).next().css({"color": "#777","text-decoration": "line-through"});
+			$("#item_left").html("还有0个未完成项目");
+			$("#bottom_right").css("display","block").children().html("清除"+$("#item_list li").length+"个已完成项目");
+		}
+		else{
+			$(".itemdone").prop("checked",false).next().css({"color": "#000","text-decoration": "none"});
+			$("#item_left").html("还有"+$("#item_list li").length+"个未完成项目");
+			$("#bottom_right").css("display","none");
+		}
+	});
+	$("#item_list").on("click",".itemdone",function(){
+		if ($(this).prop("checked") == true) 
+			$(this).next().css({"color": "#777","text-decoration": "line-through"});
+		else
+			$(this).next().css({"color": "#000","text-decoration": "none"});
+		var a = $('input:checked').length;
+		var b = $("#item_list li").length-a;
+		$("#item_left").html("还有"+b+"个未完成项目");		 
+		if (a==0) 
+			$("#bottom_right").css("display","none");
+		else
+			$("#bottom_right").css("display","block").children().html("清除"+a+"个已完成项目").click(function(){
+				if (b==0){
+					addcontent.css("display","none");
+				 	bottom.css("display","none");
+				 }
+				else{
+					$("input:checked").parents("li").remove();
+					$("#bottom_right").css("display","none");
+				}
+			});
 	});
 });
